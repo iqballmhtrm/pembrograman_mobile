9e2dcbaca3e67e47da882b89f07acfe9c8cb6bca
iqbal <iqbalmuctar@gmail.com>
2024-09-19 07:41:26 +0700

    Bag2/Praktikum1.

---
+++ bag2/praktikum1.dart
@@ -0,0 +1,14 @@
+void main() {
+  String test = "test2";
+  if (test == "test1") {
+    print("Test1");
+  } else if (test == "test2") {
+    print("Test2");
+  } else {
+    print("Something else");
+  }
+  String test1 = "true";
+  if (test1 == "true") {
+    print("Kebenaran");
+  }
+}
