29bd9546ecde81ad961cd56a71539457a34d1e40
iqbal <iqbalmuctar@gmail.com>
2024-10-02 23:36:25 +0700

    praktikum 2 - langkah 3

---
+++ bag3/praktikum2.dart
@@ -0,0 +1,20 @@
+void main() {
+  // ! LANGKAH 1
+  // var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
+  // print(halogens);
+
+  // ! LANGKAH 2
+  var names1 = <String>{};
+  Set<String> names2 = {};
+
+  // var names3 = {};
+
+  names1.add("Muhammad Iqbal Muhtaram");
+  names1.add("2241720265");
+
+  names2.addAll({"2241720265", "2241720265"});
+
+  print(names1);
+  print(names2);
+  // print(names3);
+}
